b8c9060a4bcc5994af0221c4e9e83ace8ae89296
Susheatee <88378854+Susheate@users.noreply.github.com>
2026-04-08 15:32:21 +0000

    Use consistent home directory "calls"

--- brl-nvidia.sh
+++ brl-nvidia.sh
@@ -6,7 +6,7 @@ fi
 
 if [ $1 == "install" ]; then
 	driverVersion=$(nvidia-smi | grep  "Driver Version" | cut -d ' ' -f 3)
-	if [ ! -e ~/brl-nvidia/nvidia-${driverVersion}.run ]; then curl https://us.download.nvidia.com/XFree86/Linux-x86_64/${driverVersion}/NVIDIA-Linux-x86_64-${driverVersion}.run -o ~/brl-nvidia/nvidia-${driverVersion}.run; fi
+	if [ ! -e "$HOME/brl-nvidia/nvidia-${driverVersion}.run" ]; then curl https://us.download.nvidia.com/XFree86/Linux-x86_64/${driverVersion}/NVIDIA-Linux-x86_64-${driverVersion}.run -o ~/brl-nvidia/nvidia-${driverVersion}.run; fi
 	if [ $2 != "all" ]; then
 		sudo strat -r $2 sh ~/brl-nvidia/nvidia-${driverVersion}.run --no-kernel-modules
 	else
