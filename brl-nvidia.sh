714def14cc299b1b7f0966673f35c9a49bdecac1
Susheatee <88378854+Susheate@users.noreply.github.com>
2026-04-10 10:43:17 +0000

    Refactor
    Add checksum error handling in order to re-download drivers
    Remove sudo dependency (script must be run as root)
    Refactor script into functions

--- brl-nvidia.sh
+++ brl-nvidia.sh
@@ -1,21 +1,43 @@
-#/usr/bin/bash
+#!/bin/bash
 
-if [ ! -e "$HOME/brl-nvidia" ]; then
-	mkdir ~/brl-nvidia
+initStratum=$(brl which 1)
+usedDir="/bedrock/strata/${initStratum}/var/tmp" # Change later
+driverVersion=$(nvidia-smi | grep  "Driver Version" | cut -d ' ' -f 3)
+targetedStratum=$2
+
+function downloadDrivers() {
+	curl https://us.download.nvidia.com/XFree86/Linux-x86_64/$driverVersion/NVIDIA-Linux-x86_64-$driverVersion.run -o $usedDir/brl-nvidia/nvidia-$driverVersion.run
+}
+
+function checksumReDownloadDrivers() {
+	if [[ $? == "2" ]]; then
+		echo "Re-downloading the drivers..."
+		downloadDrivers
+		installDrivers
+	fi
+}
+
+function installDrivers {
+	
+	if [[ ! -e "${usedDir}/brl-nvidia/nvidia-${driverVersion}.run" ]]; then downloadDrivers; fi
+	if [[ $targetedStratum == "all" ]]; then
+		for stratum in $(brl list)
+		do
+			if [[ $stratum != $initStratum ]] && [[ $stratum != "bedrock" ]]; then echo "${stratum}"; strat -r $stratum sh $usedDir/brl-nvidia/nvidia-$driverVersion.run --no-kernel-modules; fi
+			checksumReDownloadDrivers
+		done
+	else
+		strat -r $targetedStratum sh $usedDir/brl-nvidia/nvidia-$driverVersion.run --no-kernel-modules
+		checksumReDownloadDrivers
+	fi
+}
+
+if [[ ! -e "${usedDir}/brl-nvidia" ]]; then
+	mkdir $usedDir/brl-nvidia
 fi
 
 if [ $1 == "install" ]; then
-	driverVersion=$(nvidia-smi | grep  "Driver Version" | cut -d ' ' -f 3)
-	if [ ! -e "$HOME/brl-nvidia/nvidia-${driverVersion}.run" ]; then curl https://us.download.nvidia.com/XFree86/Linux-x86_64/${driverVersion}/NVIDIA-Linux-x86_64-${driverVersion}.run -o ~/brl-nvidia/nvidia-${driverVersion}.run; fi
-	if [ $2 != "all" ]; then
-		sudo strat -r $2 sh ~/brl-nvidia/nvidia-${driverVersion}.run --no-kernel-modules
-	else
-		for stratum in $(brl list)
-		do
-			if [ $stratum != $(brl which 1) ]; then sudo strat -r $stratum sh ~/brl-nvidia/nvidia-${driverVersion}.run --no-kernel-modules; fi
-		done
-	fi
-
-elif [ $1 == "remove" ]; then
-	sudo strat -r $2 nvidia-uninstall
+	installDrivers
+elif [[ $1 == "remove" ]]; then
+	strat -r $targetedStratum nvidia-uninstall
 fi
