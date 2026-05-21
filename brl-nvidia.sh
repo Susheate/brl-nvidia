3331ecddafc7817cb4e204971db1b909fbd9ff02
Susheatee <88378854+Susheate@users.noreply.github.com>
2026-05-21 16:21:52 +0000

    checksumDownloadDrivers becomes integrityCheck!

--- brl-nvidia.sh
+++ brl-nvidia.sh
@@ -14,7 +14,7 @@ function downloadDrivers() {
 	fi
 }
 
-function checksumReDownloadDrivers() {
+function integrityCheck() {
 	if [[ $? == "2" ]]; then
 		echo "Re-downloading the drivers..."
 		downloadDrivers force
@@ -28,66 +28,11 @@ function installDrivers {
 		for stratum in $(brl list)
 		do
 			if [[ $stratum != $initStratum ]] && [[ $stratum != "bedrock" ]] && [[ $stratum != "bpt" ]]; then echo "${stratum}"; strat -r $stratum sh $usedDir/brl-nvidia/nvidia-$driverVersion.run --no-kernel-modules; fi
-			checksumReDownloadDrivers
+			integrityCheck
 		done
 	else
 		strat -r $targetedStratum sh $usedDir/brl-nvidia/nvidia-$driverVersion.run --no-kernel-modules
-		checksumReDownloadDrivers
-	fi
-}
-
-if [[ ! -e "${usedDir}/brl-nvidia" ]]; then
-	mkdir $usedDir/brl-nvidia
-fi
-
-if [[ $1 == "install" ]]; then
-	installDrivers
-elif [[ $1 == "remove" ]]; then
-	strat -r $targetedStratum nvidia-uninstall
-elif [[ $1 == "install-script" ]]; then
-	cp $0 /bedrock/strata/${initStratum}/bin/brl-nvidia
-	chmod +x /bedrock/strata/${initStratum}/bin/brl-nvidia
-elif [[ $1 == "update-script" ]]; then
-	curl https://raw.githubusercontent.com/Susheate/brl-nvidia/refs/heads/main/brl-nvidia.sh -o $usedDir/brl-nvidia/brl-nvidia.sh
-	cp $usedDir/brl-nvidia/brl-nvidia.sh /bedrock/strata/${initStratum}/bin/brl-nvidia
-	chmod +x /bedrock/strata/${initStratum}/bin/brl-nvidia
-	echo -e "\n\e[36mDone\e[0m"
-fi
-#!/bin/bash
-
-if [ $(id -u) != 0 ]; then echo "brl-nvidia requires root."; exit 2; fi
-
-initStratum=$(brl which 1)
-usedDir="/bedrock/strata/${initStratum}/var/tmp"
-driverVersion=$(nvidia-smi | grep  "Driver Version" | cut -d ' ' -f 3)
-targetedStratum=$2
-
-
-function downloadDrivers() {
-	if [[ ! -e "${usedDir}/brl-nvidia/nvidia-${driverVersion}.run" ]] || [[ $1 == "force" ]]; then
-		curl https://us.download.nvidia.com/XFree86/Linux-x86_64/$driverVersion/NVIDIA-Linux-x86_64-$driverVersion.run -o $usedDir/brl-nvidia/nvidia-$driverVersion.run
-	fi
-}
-
-function checksumReDownloadDrivers() {
-	if [[ $? == "2" ]]; then
-		echo "Re-downloading the drivers..."
-		downloadDrivers
-		installDrivers
-	fi
-}
-
-function installDrivers {
-	downloadDrivers
-	if [[ $targetedStratum == "all" ]] || [[ $targetedStratum == "" ]]; then
-		for stratum in $(brl list)
-		do
-			if [[ $stratum != $initStratum ]] && [[ $stratum != "bedrock" ]] && [[ $stratum != "bpt" ]]; then echo "${stratum}"; strat -r $stratum sh $usedDir/brl-nvidia/nvidia-$driverVersion.run --no-kernel-modules; fi
-			checksumReDownloadDrivers
-		done
-	else
-		strat -r $targetedStratum sh $usedDir/brl-nvidia/nvidia-$driverVersion.run --no-kernel-modules
-		checksumReDownloadDrivers
+		integrityCheck
 	fi
 }
 
