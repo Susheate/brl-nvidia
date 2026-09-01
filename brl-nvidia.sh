1cfd6b3c9ea3adb22712b15d35f469dc22b89775
Susheatee <88378854+Susheate@users.noreply.github.com>
2026-09-01 21:28:36 +0200

    Make nvidia driver install script usage silent

--- brl-nvidia.sh
+++ brl-nvidia.sh
@@ -3,13 +3,13 @@
 if [ $(id -u) != 0 ]; then echo "brl-nvidia requires root."; exit 1; fi
 
 initStratum=$(brl which 1)
-usedDir="/bedrock/strata/${initStratum}/var/tmp"
+usedDir="/bedrock/strata/$initStratum/var/tmp"
 driverVersion=$(nvidia-smi | grep "KMD Version" | cut -d " " -f 3)
 targetedStratum=$2
 
 
 function downloadDrivers() {
-	if [[ ! -e "${usedDir}/brl-nvidia/nvidia-${driverVersion}.run" ]] || [[ $1 == "force" ]]; then
+	if [[ ! -e "$usedDir/brl-nvidia/nvidia-$driverVersion.run" ]] || [[ $1 == "force" ]]; then
 		curl https://us.download.nvidia.com/XFree86/Linux-x86_64/$driverVersion/NVIDIA-Linux-x86_64-$driverVersion.run -o $usedDir/brl-nvidia/nvidia-$driverVersion.run
 	fi
 }
@@ -27,13 +27,13 @@ function installDrivers() {
 	if [[ $1 == "all" ]] || [[ $1 == "" ]]; then
 		for stratum in $(brl list); do
 			if [[ $stratum != $initStratum ]] && [[ $stratum != "bedrock" ]] && [[ $stratum != "bpt" ]]; then
-				echo -e "\e[35m$stratum\e[0m"
-				strat -r $stratum sh $usedDir/brl-nvidia/nvidia-$driverVersion.run --no-kernel-modules
+				echo -e "\e[36mProceeding with\e[36m \e[35m$stratum\e[0m"
+				strat -r $stratum sh $usedDir/brl-nvidia/nvidia-$driverVersion.run --no-kernel-modules -q --ui=none --no-x-check
 			fi
 			integrityCheck
 		done
 	else
-		strat -r $1 sh $usedDir/brl-nvidia/nvidia-$driverVersion.run --no-kernel-modules
+		strat -r $1 sh $usedDir/brl-nvidia/nvidia-$driverVersion.run --no-kernel-modules -q --ui=none --no-x-check
 		integrityCheck
 	fi
 }
@@ -42,8 +42,8 @@ function removeDrivers() {
 	if [[ $1 == "all" ]] || [[ $1 == "" ]]; then
 		for stratum in $(brl list); do
 			if [[ $stratum != $initStratum ]] && [[ $stratum != "bedrock" ]] && [[ $stratum != "bpt" ]]; then
-				echo -e "\e[35m$stratum\e[0m"
-				strat -r $stratum nvidia-uninstall
+				echo -e "\e[36mProceeding with\e[36m \e[35m$stratum\e[0m"
+				strat -r $stratum nvidia-uninstall -q --ui=none
 			fi
 		done
 	else
@@ -56,8 +56,8 @@ function updateDrivers() {
 	for stratum in $(brl list); do
 		strat $stratum nvidia-smi
 		if [[ $? == 0 ]] && [[ $stratum != $initStratum ]] && [[ $stratum != "bedrock" ]] && [[ $stratum != "bpt" ]]; then
-			echo -e "\e[35m$stratum\e[0m"
-			strat -r $stratum sh $usedDir/brl-nvidia/nvidia-$driverVersion.run --no-kernel-modules --ui=none --no-questions --no-x-check
+			echo -e "\e[36mProceeding with\e[36m \e[35m$stratum\e[0m"
+			strat -r $stratum sh $usedDir/brl-nvidia/nvidia-$driverVersion.run --no-kernel-modules -q --ui=none --no-x-check
 		else
 			echo -e "\e[36mSkipping\e[36m \e[35m$stratum\e[0m"
 		fi
@@ -65,32 +65,32 @@ function updateDrivers() {
 	done
 }
 
-if [[ ! -e "${usedDir}/brl-nvidia" ]]; then
+if [[ ! -e "$usedDir/brl-nvidia" ]]; then
 	mkdir $usedDir/brl-nvidia
 fi
 
 case $1 in
 	"install")
 		installDrivers $targetedStratum
-		echo -e "\n\e[36mDone\e[0m"
+		echo -e "\e[36mDone\e[0m"
 		;;
 	"remove")
 		removeDrivers $targetedStratum
-		echo -e "\n\e[36mDone\e[0m"
+		echo -e "\e[36mDone\e[0m"
 		;;
 	"update")
 		updateDrivers
-		echo -e "\n\e[36mDone\e[0m"
+		echo -e "\e[36mDone\e[0m"
 		;;
 	"install-script")
 		cp $0 /bedrock/strata/${initStratum}/bin/brl-nvidia
 		chmod +x /bedrock/strata/${initStratum}/bin/brl-nvidia
-		echo -e "\n\e[36mDone\e[0m"
+		echo -e "\e[36mDone\e[0m"
 		;;
 	"update-script")
 		curl https://raw.githubusercontent.com/Susheate/brl-nvidia/refs/heads/main/brl-nvidia.sh -o $usedDir/brl-nvidia/brl-nvidia.sh
 		cp $usedDir/brl-nvidia/brl-nvidia.sh /bedrock/strata/${initStratum}/bin/brl-nvidia
 		chmod +x /bedrock/strata/${initStratum}/bin/brl-nvidia
-		echo -e "\n\e[36mDone\e[0m"
+		echo -e "\e[36mDone\e[0m"
 		;;
 esac
